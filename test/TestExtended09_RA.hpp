98f49da1f3b6672eed7f5d15d7982c347d32ed96
AvciRecep <41246465+AvciRecep@users.noreply.github.com>
2021-03-29 11:50:55 +1300

    Update TestExtended09_RA.hpp

--- test/TestExtended09_RA.hpp
+++ test/TestExtended09_RA.hpp
@@ -197,7 +197,7 @@ private:
         HeartConfig::Instance()->SetVisualizeWithMeshalyzer(true);
         HeartConfig::Instance()->SetVisualizeWithVtk(false);
 
-        HeartConfig::Instance()->SetOutputDirectory("Stomach3D_dt1000ms_10s_v3");
+        HeartConfig::Instance()->SetOutputDirectory("Stomach3D_dt1000ms_10s_noOrtho");
         HeartConfig::Instance()->SetOutputFilenamePrefix("results");
         
         //HeartConfig::Instance()->SetMeshFileName("projects/mesh/Stomach3D/H09ext_4_4_1.1", cp::media_type::Orthotropic);
