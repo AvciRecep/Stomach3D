5fc693a9c0914ffc982f4f903d5423dc9fd6394e
AvciRecep <41246465+AvciRecep@users.noreply.github.com>
2021-05-12 17:11:30 +1200

    Update TestExtended09_Rat.hpp

--- test/TestExtended09_Rat.hpp
+++ test/TestExtended09_Rat.hpp
@@ -188,14 +188,14 @@ private:
         HeartConfig::Instance()->SetUseAbsoluteTolerance(2e-1);
         HeartConfig::Instance()->SetKSPPreconditioner("jacobi");
 
-        HeartConfig::Instance()->SetSimulationDuration(120000);  //ms.
+        HeartConfig::Instance()->SetSimulationDuration(60000);  //ms.
 
         // Output visualization options, we ask for meshalyzer and cmgui
         HeartConfig::Instance()->SetVisualizeWithCmgui(false);
         HeartConfig::Instance()->SetVisualizeWithMeshalyzer(true);
         HeartConfig::Instance()->SetVisualizeWithVtk(false);
 
-        HeartConfig::Instance()->SetOutputDirectory("Stomach3D_rat_16_16_1_dt1s_120s_0001_001_01");
+        HeartConfig::Instance()->SetOutputDirectory("Stomach3D_rat_16_16_1_dt1s_60s_0005_005_05");
         HeartConfig::Instance()->SetOutputFilenamePrefix("results");
 
         HeartConfig::Instance()->SetMeshFileName("projects/mesh/Stomach3D/rat_16_16_1.1", cp::media_type::Orthotropic);
